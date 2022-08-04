80a85944e8f097ab72c1e109cfd73afe1e74eb82
nenita.espinosa <nenita_e@LAPTOP-12IE6UI1.localdomain>
2022-08-04 19:34:36 -0400

    added prob 08

--- starter/problems/08-value-pair.js
+++ starter/problems/08-value-pair.js
@@ -12,7 +12,19 @@ valuePair(object1, object2, 'name'); // => [ 'One', 'Two' ]
 
 function valuePair(obj1, obj2, key) {
   // your code here
-}
+  let newArr= [];
+  let key1 = obj1[key];
+  let key2 = obj2[key];
 
+    newArr.push(key1);
+    newArr.push(key2);
+    //console.log(newArr)
+    return newArr;
+
+}
+let object1 = {name: 'One', location: 'NY', age: 3};
+let object2 = {name: 'Two', location: 'SF'};
+valuePair(object1, object2, 'location'); // => [ 'NY', 'SF' ]
+valuePair(object1, object2, 'name'); // => [ 'One', 'Two' ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = valuePair;
