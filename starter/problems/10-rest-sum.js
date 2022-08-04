28246103fe9401ca6c55186776ef5ad0e291c7a9
nenita.espinosa <nenita_e@LAPTOP-12IE6UI1.localdomain>
2022-08-04 19:56:39 -0400

    added prob 10

--- starter/problems/10-rest-sum.js
+++ starter/problems/10-rest-sum.js
@@ -10,9 +10,17 @@ restSum(0); // => 0
 ***********************************************************************/
 
 // you'll need to change the parameters!
-function restSum() {
+function restSum(...everything) {
   // your code here
-}
+  let sum = 0;
+  for (let i = 0; i <everything.length; i++) {
+    sum += everything[i];
+  }
+  return sum;
 
+}
+restSum(3,5,6); // => 14
+restSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 14
+restSum(0); // => 0
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = restSum;
